1d2100a5fb034be6f456458c003095823ecb87cf
ct2sjk <Ct2sjk@gmail.com>
2023-12-13 21:50:08 +0000

    Added scene

--- countdown.gd
+++ countdown.gd
@@ -1,1 +1,18 @@
 extends Label
+
+#amount the timer starts at
+@export var starttime = 60
+
+#multiplier for time subtraction
+@export var timerate = 1.0 
+
+#time left in level
+var time = 0
+
+#initialize time variable value with level start
+func _levelstart():
+	time = starttime
+	
+#update time on every frame
+func _process(delta):
+	time -= delta
