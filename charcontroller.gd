d3e372031ff2371f1090c45d07880bf009ae78c7
ct2sjk <Ct2sjk@gmail.com>
2023-12-14 15:08:35 +0000

    character controller added

---
+++ charcontroller.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+@export var movespeed : float = 100
+@export var starting_direction : Vector2 = Vector2(0,1)
+#parameters/idle/blend_position
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
+func _ready():
+	print("hello")
+#	update_animation_parameters(starting_direction)
+	
+func _physics_process(_delta):
+	
+	var input_direction = Vector2(
+		Input.get_action_strength("Right") - Input.get_action_strength("Left"),
+		Input.get_action_strength("Down") - Input.get_action_strength("Up")
+	)
+	
+#	update_animation_parameters(input_direction)
+	
+	velocity = input_direction * movespeed
+	
+	move_and_slide()
+
+
+
+#	pick_new_state()
+#func update_animation_parameters(move_input : Vector2):
+#	if (move_input != Vector2.ZERO):
+#		animation_tree.set("parameters/walk/blend_position", move_input)
+#		animation_tree.set("parameters/idle/blend_position", move_input)
+#		
+#func pick_new_state():
+#	if (velocity != Vector2.ZERO):
+#		state_machine.travel("walk")
+#	else:
+#		state_machine.travel("idle")
--- countdown.gd
+++ countdown.gd
@@ -1,18 +1,22 @@
 extends Label
 
 #amount the timer starts at
-@export var starttime = 60
+@export var starttime = 60.0
 
 #multiplier for time subtraction
 @export var timerate = 1.0 
 
 #time left in level
-var time = 0
+var time = 0.0
 
 #initialize time variable value with level start
-func _levelstart():
+func _ready():
 	time = starttime
 	
 #update time on every frame
 func _process(delta):
 	time -= delta
+	if time < 0:
+		print("timeup")
+	text = "%02d" % time
+	
